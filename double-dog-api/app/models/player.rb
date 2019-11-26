456e662182954c4347eb1e7a77997ad5f6914694
Mars <marsbhoward@gmail.com>
2019-11-25 22:18:00 -0500

    added model assosassociations

--- double-dog-api/app/models/player.rb
+++ double-dog-api/app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ApplicationRecord
+	has_many :player_turns
+	has_many :turns, through: :player_turns
+
+	validates :name, presence: true
 end
--- double-dog-api/app/models/turn.rb
+++ double-dog-api/app/models/turn.rb
@@ -1,2 +1,4 @@
 class Turn < ApplicationRecord
+	has_many :player_turns
+	has_many :players, through: :player_turns
 end
