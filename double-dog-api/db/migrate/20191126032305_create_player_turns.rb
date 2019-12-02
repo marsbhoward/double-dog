eb957ed1a7d440dfc364d88cd14b250ce391392a
Mars <marsbhoward@gmail.com>
2019-12-02 16:37:40 -0500

    add game to player turn

--- double-dog-api/db/migrate/20191126032305_create_player_turns.rb
+++ double-dog-api/db/migrate/20191126032305_create_player_turns.rb
@@ -3,6 +3,7 @@ class CreatePlayerTurns < ActiveRecord::Migration[6.0]
     create_table :player_turns do |t|
       t.references :player,null: false, foreign_key: true
       t.references :dare,null: false, foreign_key: true
+      t.references :game,null: false, foreign_key: true
 
       t.timestamps
     end
